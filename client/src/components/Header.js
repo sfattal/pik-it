8b9be2467380008357a5ea4e3e9282f412c44946
ischdry <ichowdhury4@fordham.edu>
2019-03-19 19:50:46 -0400

    navbar, form basic landing page

---
+++ client/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react"
+import {Navbar,Nav,NavDropdown,FormControl,Form,Button} from "react-bootstrap" 
+
+
+function Header () {
+  return (
+      <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="float-right">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>   
+      </Nav>
+      
+    </Navbar.Collapse>
+  </Navbar>
+   )
+  
+}
+
+export default Header; 
