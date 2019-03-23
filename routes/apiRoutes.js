26e4705128372145c900a2946d5abd4376dba469
ogammal <gammal93@mac.com>
2019-03-23 13:30:38 -0400

    Create initial api routes and seed db

---
+++ routes/apiRoutes.js
@@ -0,0 +1,34 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  app.get("/api/polls", function(req, res) {
+    db.Polls.findAll({
+    }).then(function(dbPolls) {
+      console.log(dbPolls)
+      res.json(dbPolls);
+    });
+  });
+
+//   app.get("/api/authors/:id", function(req, res) {
+//     db.Choice.findOne({
+//         where: {
+//         id: req.params.id
+//         },
+//         include: [db.Poll]
+//     }).then(function(dbChoice) {
+//         res.json(dbChoice);
+//     });
+// });
+
+//   app.get("/api/results/:pollid"), function(req, res) {
+//       db.Response.findAll({
+//           where: {
+//               poll_id: pollid
+//           },
+//           include: [db.Choice]
+//       }).then(function(dbResult) {
+//         console.log(dbResult)
+//           res.json(dbResult)
+//       })
+//   }
+}
