172dbd12c6796353f0a161b3d497816f831d0bab
sepanther <sepanta.mohseni@gmail.com>
2019-03-26 22:47:08 -0400

    fix ordering logic

--- controllers/pollController.js
+++ controllers/pollController.js
@@ -2,16 +2,49 @@
 //     Responses.findAll({ where: { poll_id = pollID } })
 // }
 
-var results = {}
+var rankings = {}
 
-function determineChoices(repsonses) {
-    for (i=0; i<responses.length; i++) {
-    
-    }
+function determineChoices(poll_id) {
+    db.Choice.findAll({
+        where: {
+            poll_id: poll_id
+        }
+    }).then(function(choices) {
+        // console.log(choices);
+        for (i=0; i<choices.length; i++) {
+            var index = (choices[i].dataValues.id);
+            rankings[index] = 0;
+        }
+        console.log(rankings);
+    })
+    determineRankings(rankings, poll_id);
 }
 
-function determineRankings(responses) {
 
+function determineRankings(ranks, poll_id) {
+    db.Response.findAll({
+        where: {
+            poll_id: poll_id
+        }
+    }).then(function(dbResult) {
+        // console.log(dbResult);
+        console.log("ranks: " + ranks)
+        for (let key in ranks) {
+            console.log(key)
+            for (j=0; j<dbResult.length; j++) {
+                console.log(dbResult[j].dataValues)
+                var choiceID = parseInt(dbResult[j].dataValues.choice_id)
+                // console.log("choice ID: " + choiceID)
+                // console.log(choiceID)
+                var rank = parseInt(dbResult[j].dataValues.rank)
+                // console.log("rank: " + rank)
+                if ( parseInt(key) === choiceID) {
+                    ranks[key] += rank;
+                }
+            }
+        }
+    console.log(rankings);
+    })
 }
 
 const db = require("../models");
@@ -19,14 +52,16 @@ const db = require("../models");
 // Defining methods for the booksController
 module.exports = {
   doAllTheWork: function(req, res) {
+      var poll_id = req.params.pollid
     db.Response.findAll({
         where: {
             poll_id: req.params.pollid
         },
         include: [db.Choice]
     }).then(function(dbResult) {
-      console.log(dbResult)
-      determineRankings(dbResult)
+    //   console.log(dbResult)
+      determineChoices(req.params.pollid)
+    //   determineRankings(rankings, poll_id)
         res.json(dbResult)
     })
   }
