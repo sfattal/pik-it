4971c3a3bbf20aa3d15c55a0ba0bf19f410cdbc8
ogammal <gammal93@mac.com>
2019-03-27 18:35:53 -0400

    fix some merge conflicts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,35 +12,6 @@ module.exports = function(app) {
       res.send(dbPolls);
     });
   });
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-  app.get("/api/polls/:id", function(req, res) {
-    db.Choice.findOne({
-        where: {
-        id: req.params.id
-        },
-        include: [db.Poll]
-    }).then(function(dbChoice) {
-        res.json(dbChoice);
-    });
-});
-
-  app.get("/api/results/:pollid"), function(req, res) {
-      db.Response.findAll({
-          where: {
-              poll_id: pollid
-          },
-          include: [db.Choice]
-      }).then(function(dbResult) {
-        console.log(dbResult)
-          res.json(dbResult)
-      })
-  }
-=======
-=======
-  
->>>>>>> 303e8f731a7a641b904bad74b68b0afeaca4fdc7
 
 //   app.get("/api/authors/:id", function(req, res) {
 //     db.Choice.findOne({
@@ -55,5 +26,4 @@ module.exports = function(app) {
 
   app.get("/api/results/:pollid", controller.doAllTheWork)
 
->>>>>>> a282fa92019cb554cdea82a313bb56f9d86115f6
 }
