f93b029c650d5c61b52eece026a22c352429c59e
sfattal <fattal.sherif@gmail.com>
2019-03-29 16:51:43 -0400

    deleteChoice function works, needs logic

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -1,11 +1,6 @@
 import React, {Component} from 'react'
 import Container from '../components/Container'
 import {Form} from '../components/Form/Form'
-
-// import '../components/Form/css/custom.css'
-// import '../components/Form/css/normalize.css'
-// import '../components/Form/css/prog-tracker.css'
-// import '../components/Form/css/skeleton.css'
 import './pages.css'
 
 class Create extends Component {
