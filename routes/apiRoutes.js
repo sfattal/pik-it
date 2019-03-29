0967802b9048317a61d10b11c2b805da17b8daa3
sepanther <sepanta.mohseni@gmail.com>
2019-03-28 23:22:40 -0400

    make some routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,7 @@
 var db = require("../models");
+var router = require("express").Router
 var controller = require("../controllers/pollController.js")
+
 
 module.exports = function(app) {
   app.get("/api/polls", function(req, res) {
@@ -11,25 +13,12 @@ module.exports = function(app) {
     });
   });
   
-//   app.get("/api/authors/:id", function(req, res) {
-//     db.Choice.findOne({
-//         where: {
-//         id: req.params.id
-//         },
-//         include: [db.Poll]
-//     }).then(function(dbChoice) {
-//         res.json(dbChoice);
-//     });
-// });
   app.get("/api/polls/:pollkey", controller.getPollChoices)
 
   app.get("/api/results/:pollid", controller.doAllTheWork)
-<<<<<<< HEAD
 
   app.post("/api/sendPollData", controller.sendPollData);
 
   app.post("/api/sendResponseData", controller.sendResponseData);
 
-=======
->>>>>>> ec558e047180c7916008e11d9d54ff9813f78ec5
 }
