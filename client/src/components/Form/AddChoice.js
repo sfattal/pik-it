8af2ec7a290139931229cabfe28a5c722b56f335
ischdry <ichowdhury4@fordham.edu>
2019-03-29 20:45:41 -0400

    css changes

--- client/src/components/Form/AddChoice.js
+++ client/src/components/Form/AddChoice.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 function AddChoice(props) {
     return (
-        <div>
-            <div style={{display: !props.choice && "none"}}>
+        <div class ="row pt-2">
+            <div class ="col-3" style={{display: !props.choice && "none"}}>
                 <p>{props.choice}</p>
             </div>
+            <div class ="col-3"> 
             <button id="delete" style={{display: !props.choice && "none"}} onClick={props.deleteChoice}>Delete</button>
+            </div>
         </div>
     )
 }
--- client/src/components/Form/StepTwo.js
+++ client/src/components/Form/StepTwo.js
@@ -4,26 +4,28 @@ import AddChoice from './AddChoice'
 function StepTwo(props) {
   console.log(props) //REMOVE
   return (
-    <div>
-      <div className='row'>
+    <div className = "container">
+      <div className='fluid-container'>
+          <div className ="row"> 
+                <div className='col-8'>
+                    <input
+                    className='u-full-width'
+                    placeholder='Example Choice'
+                    type='text'
+                    onChange={props.handleChoiceChanged}
+                    value={props.choice}
+                    autoFocus
+                    />
+              </div>
+              <div className='col-4'>
+                <button onClick={props.addChoice}>+</button>
+              </div>
+        </div>
+        <div/>
         {props.renderAddChoices()}
-        <AddChoice />
       </div>
-      <label>Choice</label>
       <div className='row'>
-        <div className='col-11'>
-          <input
-            className='u-full-width'
-            placeholder='Example Choice'
-            type='text'
-            onChange={props.handleChoiceChanged}
-            value={props.choice}
-            autoFocus
-          />
-        </div>
-        <div className='col-1'>
-          <button onClick={props.addChoice}>+</button>
-        </div>
+        
         <div className='row'>
           <div className='col-12'>
             <button onClick={props.setPageBack}>Back</button>
