ca9cd0507e06ed04afb02b3c27730499e1eebde1
sfattal <fattal.sherif@gmail.com>
2019-03-30 11:12:55 -0400

    functioning reroute after submit

--- client/src/components/Dash/AllChoices.js
+++ client/src/components/Dash/AllChoices.js
@@ -4,12 +4,16 @@ function AllChoices(props) {
     console.log(props)
     return (
         <div className="allChoices">
-            { props.choices.map((choice, index) => {
+            { props.choices.map((choice) => {
+                console.log(choice)
                 return (
                     <button 
-                    key={props.choices[index].id}
-                    choiceText={props.choices[index].choice_text}
-                    onClick={props.sortChoice}>{choice}</button>
+                    key={choice.id}
+                    value={choice.choice_text}
+                    choiceText={choice.choice_text}
+                    onClick={props.sortChoice}>
+                    {choice.choice_text}
+                    </button>
                 )
             })}
         </div>
@@ -17,20 +21,3 @@ function AllChoices(props) {
 }
 
 export default AllChoices
-
-// GET from the db for each choice name and ID
-
-// import React, {Component} from 'react'
-// import LoadingHOC from './LoadingHOC'
-// import '../styles/main.css'
- 
-// const List = (props) =>{
-//   const{usernames} = props
-//   return(
-//     <ul>
-//       {usernames.map(user => <li key={user.id}>{user.name}</li>)}
-//     </ul>
-//   )
-// }
- 
-// export default LoadingHOC(List)
--- controllers/pollController.js
+++ controllers/pollController.js
@@ -10,7 +10,6 @@ function determineChoices(poll_id) {
             poll_id: poll_id
         }
     }).then(function(choices) {
-        // console.log(choices);
         for (i=0; i<choices.length; i++) {
             var index = (choices[i].dataValues.id);
             rankings[index] = 0;
@@ -27,17 +26,13 @@ function determineRankings(ranks, poll_id) {
             poll_id: poll_id
         }
     }).then(function(dbResult) {
-        // console.log(dbResult);
         console.log("ranks: " + ranks)
         for (let key in ranks) {
             console.log(key)
             for (j=0; j<dbResult.length; j++) {
                 console.log(dbResult[j].dataValues)
                 var choiceID = parseInt(dbResult[j].dataValues.choice_id)
-                // console.log("choice ID: " + choiceID)
-                // console.log(choiceID)
                 var rank = parseInt(dbResult[j].dataValues.rank)
-                // console.log("rank: " + rank)
                 if ( parseInt(key) === choiceID) {
                     ranks[key] += rank;
                 }
@@ -52,14 +47,12 @@ const db = require("../models");
 // Defining methods for the controller
 module.exports = {
   doAllTheWork: function(req, res) {
-    var poll_id = req.params.pollid
     db.Response.findAll({
         where: {
             poll_id: req.params.pollid
         },
         include: [db.Choice]
     }).then(function(dbResult) {
-    //   console.log(dbResult)
       determineChoices(req.params.pollid)
     //   determineRankings(rankings, poll_id)
         res.json(dbResult)
@@ -94,9 +87,9 @@ module.exports = {
               choice_text: choice
           })
         })
-        .then(function(dbResult) {
-            res.send(dbResult.poll_id)
-        })
+        setTimeout(() => {
+            res.send({pollkey: req.body.poll_key})
+        }, 1000)
     },
     sendPollData: function(req, res) {
       console.log("posting to poll");
