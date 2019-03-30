b6bf475ca1bf444efbb707788cda74b1f68aac78
sfattal <fattal.sherif@gmail.com>
2019-03-30 00:57:59 -0400

    user selection and results ip

--- client/src/components/Form/StepOne.js
+++ client/src/components/Form/StepOne.js
@@ -7,8 +7,8 @@ function StepOne(props) {
         <div className='row justify-content-center d-flex'>
                     <h4 className ="text-light"> Title</h4>
           <div className='col-12 pb-3 justify-content-center d-flex'>
-              <div class="form-group col-6 justify-content-center">
-                <input  id ="title" type="text" className="form-control" id="title" placeholder="title"   onChange={props.handleTitleChanged}
+              <div className="form-group col-6 justify-content-center">
+                <input  id ="title" type="text" className="form-control" placeholder="title"   onChange={props.handleTitleChanged}
               value={props.title}
               autoFocus/>
                </div>
@@ -17,8 +17,8 @@ function StepOne(props) {
         <div className='row justify-content-center d-flex'>
               <h4 className ="text-light"> Description</h4>
           <div className = 'col-12 pb-3 justify-content-center d-flex'>
-              <div class="form-group col-6 justify-content-center">
-                  <input id ="desc" type="text" className="form-control" id="title" placeholder="description" 
+              <div className="form-group col-6 justify-content-center">
+                  <input id ="desc" type="text" className="form-control" placeholder="description" 
                   onChange={props.handleDescChanged}
                   value={props.desc}/>
                 </div>
@@ -27,8 +27,8 @@ function StepOne(props) {
           <div className='row justify-content-center d-flex'>
                 <h4 className ="text-light"> Label</h4>
                 <div className = 'col-12 pb-3 justify-content-center d-flex' >
-                    <div class="form-group col-6 justify-content-center">
-                          <input id="email" type="text" className="form-control" id="title" placeholder="title" onChange={props.handleEmailChanged}
+                    <div className="form-group col-6 justify-content-center">
+                          <input id="email" type="text" className="form-control" placeholder="title" onChange={props.handleEmailChanged}
                     value={props.email}
                     autoFocus />
                     </div>
@@ -37,7 +37,7 @@ function StepOne(props) {
         
           <div className='row'>
             <div className='col-12'>
-              <button onClick={props.setPageNext} class="btn btn-outline-light btn-lg">Next</button>
+              <button onClick={props.setPageNext} className="btn btn-outline-light btn-lg">Next</button>
             </div>
           </div>
         </div> 
--- client/src/pages/Results.js
+++ client/src/pages/Results.js
@@ -1,9 +1,29 @@
-// import React, {Component} from 'react'
-// import Container from '../components/Container'
-// import './pages.css'
+import React, {Component} from 'react'
+import Container from '../components/Container'
+import Hero from '../components/Hero/index'
+import './pages.css'
 
-// class Results extends Component {
+class Results extends Component {
+    constructor () {
+        super()
+        this.state = {
+            // pending: 
+            // result: 
+        }
+        // this.handleUserOrderChanged = this.handleUserOrderChanged.bind(this);
+    }
 
-// }
+    render() {
+        return (
+            <div>
+            <Hero backgroundImage = "https://images.unsplash.com/photo-1508796079212-a4b83cbf734d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80">
+               <Container>
+                {/* {this.renderResultsPage()} */}
+               </Container>
+            </Hero>
+         </div>
+        )
+    }
+}
 
-// export default Results;
+export default Results;
