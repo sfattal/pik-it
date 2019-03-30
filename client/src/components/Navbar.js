3663b39651c48b18528e2a073fa74727352f7dab
ischdry <ichowdhury4@fordham.edu>
2019-03-30 02:50:45 -0400

    new logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import "./navbar.css"
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
-        pik-it
+                    <span> <img id ="navBarLogo" src = "https://scontent-lga3-1.xx.fbcdn.net/v/t1.15752-9/55704659_392603638134129_887138109307748352_n.png?_nc_cat=100&_nc_ht=scontent-lga3-1.xx&oh=d6f3bbb45e085891bb34877bd09d7de2&oe=5D4CE6EB"/> </span>
+
       </Link>
       <div>
         <ul className="navbar-nav">
@@ -14,7 +16,7 @@ function Navbar() {
               to="/create"
               className={window.location.pathname === "/create" ? "nav-link active" : "nav-link"}
             >
-              Create
+            Create
             </Link>
           </li>
           {/* <li className="nav-item">
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,7 +9,10 @@ class Home extends Component {
          <div>
             <Hero backgroundImage = "https://images.unsplash.com/photo-1508796079212-a4b83cbf734d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80">
                <Container>
-                  <h1 className ="pb-5">pik-it</h1>
+                  {/* <h1 className ="pb-5">pik-it</h1> */}
+                  <div> 
+                  < img id = "logoImage" src ="https://scontent-lga3-1.xx.fbcdn.net/v/t1.15752-9/55910154_313268392656091_5592172995645800448_n.png?_nc_cat=106&_nc_ht=scontent-lga3-1.xx&oh=4269119f21043d261c206ef7ebbeaabc&oe=5D4FB012"/>
+                  </div>
                   <div id="mainCard" className="card text-center pt-3">
                      <div className="card-body">
                         <h5 className="card-title">Welcome to pik-it!</h5>
