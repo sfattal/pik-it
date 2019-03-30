c5a54a314fac40ab3840f3c3368a1430b9b1a56b
ischdry <ichowdhury4@fordham.edu>
2019-03-30 12:53:59 -0400

    final update

--- client/src/components/Dash/AllChoices.js
+++ client/src/components/Dash/AllChoices.js
@@ -7,13 +7,16 @@ function AllChoices(props) {
             { props.choices.map((choice) => {
                 console.log(choice)
                 return (
+                    <div className = "row pt-3">
                     <button 
+                    className ="btn btn-outline-primary"
                     key={choice.id}
                     value={choice.choice_text}
                     choiceText={choice.choice_text}
                     onClick={props.sortChoice}>
                     {choice.choice_text}
                     </button>
+                    </div>
                 )
             })}
         </div>
--- client/src/components/Dash/ChoiceOrder.js
+++ client/src/components/Dash/ChoiceOrder.js
@@ -2,13 +2,17 @@ import React from 'react'
 
 function ChoiceOrder(props) {
     return (
-        <ol className="choiceOrder">
+        <ol className="choiceOrder fluid-container">
             { props.positions.map(position => {
                 return (
-                    <div className="row">
-                        <li>{position}</li>
-                        <button id="removeChoice">remove</button>
-                        {/* onClick={props.removeChoice} */}
+                    <div className = "row pt-1">  
+                        <div className = "col-3"> 
+                            <h6 className ="mt-2">{position}</h6>
+                        </div>
+                        <div className = "col-2">
+                             <button className ="btn btn-danger" id="removeChoice">remove</button>
+                            {/* onClick={props.removeChoice} */}
+                        </div>
                     </div>
                 )
             })}
